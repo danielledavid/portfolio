e2a7cdbdab616cfe3230121b1e5f83bec6eec2fe
Dane David <danielle.david@alumni.ateneo.edu>
2026-01-10 18:59:52 -0500

    Remove theme toggle and lock dark mode

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { ThemeToggle } from "../components/ThemeToggle";
+
 import { StarBackground } from "../components/StarBackground";
 
 import { HeroSection } from "../components/HeroSection";
@@ -10,8 +10,6 @@ import { ProjectsSection } from "../components/ProjectsSection";
 export default function Home() {
     return <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
 
-        {/* Theme Toggle */}
-        <ThemeToggle />
 
         {/* Background Effects */}
         <StarBackground />
